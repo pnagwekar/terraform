f03c6adf793b8dc6b7531072086c22f5cf306b92
pnagwekar <pnagwekar@aahanas-mbp.lan>
2022-06-06 11:29:52 -0700

    First commit

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
+  default     =  "parag-oregon"
 }
 
 variable "instance_type" {
@@ -15,12 +15,12 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "my-jenkins-security-group"
+  default     = "Parag-jenkins-security-group"
 }
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
+  default     = "Parag-ec2-instance"
 }
 
 variable "ami_id" {
